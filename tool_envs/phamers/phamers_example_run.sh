c24bff0f330595fb298e64f2c1c00b48a4cbec40
Kenneth Edward Schackart <schackartk@email.arizona.edu>
2021-07-13 14:00:34 -0700

    add phamers_example_run.sh

---
+++ tool_envs/phamers/phamers_example_run.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+### REQUIRED: 
+### Research group/PI
+#SBATCH --account=bhurwitz
+### Job queue (standard|windfall|high_pri)
+### If windfall, omit --account
+#SBATCH --partition=standard
+### Number of nodes
+#SBATCH --nodes=1
+### Number of CPUs per node 
+#SBATCH --ntasks=14
+### Amount of memory per node
+#SBATCH --mem=64gb
+### Job walltime (HHH:MM:SS)
+#SBATCH --time=24:00:00
+### OPTIONAL:
+### Job name
+### SBATCH --job-name=JobName
+### Standard output filename
+### SBATCH -o out_filename.txt
+### Standard error filename
+### SBATCH -e error_filename.txt
+### Email notifications (BEGIN|END|FAIL|ALL)
+### SBATCH --mail-type=ALL
+### Email addresss
+### SBATCH --mail-user=schackartk@email.arizona.edu
+
+# Source ~/.bashrc for aliases and $PATH
+source ~/.bashrc
+
+# Activate a conda environment
+source activate phamers_env
+
+# Load any necessary modules
+# module load my_modules
+
+# Get current directory to help establish paths
+DIR="/xdisk/bhurwitz/mig2020/rsgrps/bhurwitz/schackartk/projects/phage_finders/tool_envs/phamers"
+
+cd $DIR
+
+input_dir="../../src/genome_chopper/out"
+
+python PhaMers/scripts/phamer.py -in $input_dir -data data --debug --equalize_reference
