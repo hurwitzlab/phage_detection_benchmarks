6c8d2dbff16db409b821702e7c12733aa0a5d5db
Kenneth Schackart <schackartk1@gmail.com>
2021-09-07 19:49:35 -0700

    add tests for stratified behavior

--- src/data_selection/tests/selector_test.py
+++ src/data_selection/tests/selector_test.py
@@ -8,6 +8,7 @@ from subprocess import getstatusoutput
 
 PRG = './selector.py'
 TEST1 = 'tests/inputs/chopped/archaea/5'
+TEST2 = 'tests/inputs/chopped/archaea/50'
 
 
 # --------------------------------------------------
@@ -74,13 +75,23 @@ def test_ok() -> None:
         if os.path.isdir(out_dir):
             shutil.rmtree(out_dir)
 
-        rv, out = getstatusoutput(f'{PRG} {TEST1} -n 3 -o {out_dir}')
+        # -n less than number of files
+        rv, out = getstatusoutput(f'{PRG} {TEST1} -n 2 -o {out_dir}')
 
-        assert out == (f'Done. Wrote 3 records to {out_file}.')
+        assert out == (f'Done. Wrote 2 records to {out_file}.')
         assert rv == 0
         assert os.path.isdir(out_dir)
         assert os.path.isfile(out_file)
-        assert open(out_file).read().count('>') == 3
+        assert open(out_file).read().count('>') == 2
+
+        # -n more than number of files, with replacement
+        rv, out = getstatusoutput(f'{PRG} {TEST1} -n 20 -o {out_dir} -r')
+
+        assert out == (f'Done. Wrote 20 records to {out_file}.')
+        assert rv == 0
+        assert os.path.isdir(out_dir)
+        assert os.path.isfile(out_file)
+        assert open(out_file).read().count('>') == 20
 
     finally:
         if os.path.isdir(out_dir):
@@ -88,7 +99,7 @@ def test_ok() -> None:
 
 
 # --------------------------------------------------
-def test_warns() -> None:
+def test_warns_too_many() -> None:
     """ Warns on too large num_frags """
 
     out_dir = 'out_test'
@@ -97,13 +108,23 @@ def test_warns() -> None:
         if os.path.isdir(out_dir):
             shutil.rmtree(out_dir)
 
-        rv, out = getstatusoutput(f'{PRG} {TEST1} -n 100 -o {out_dir}')
+        # No replacement, so number of frags limited to number of files
+        rv, out = getstatusoutput(f'{PRG} {TEST1} -n 20 -o {out_dir}')
 
-        assert out.startswith('Requested number of fragments')
+        assert re.search(r'Number of requested fragments \(\d+\)', out)
         assert rv == 0
         assert os.path.isdir(out_dir)
         assert os.path.isfile(out_file)
-        assert open(out_file).read().count('>') == 72
+        assert open(out_file).read().count('>') == 2
+
+        # With replacement, number of frags limited to all frags in all files
+        rv, out = getstatusoutput(f'{PRG} {TEST2} -n 100 -o {out_dir} -r')
+
+        assert re.search(r'Unable to find \d+ unique', out)
+        assert rv == 0
+        assert os.path.isdir(out_dir)
+        assert os.path.isfile(out_file)
+        assert open(out_file).read().count('>') == 6
 
     finally:
         if os.path.isdir(out_dir):
