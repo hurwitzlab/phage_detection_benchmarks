73576dd11f44a0a4ddf7a0792165456476bc9f5f
Kenneth Schackart <schackartk1@gmail.com>
2021-11-02 13:34:51 -0700

    working version

--- src/post_proc/wrangle_virsorter.sh
+++ src/post_proc/wrangle_virsorter.sh
@@ -1,1 +1,29 @@
 #!/usr/bin/env bash
+
+# Concatenate the various sequence files from VirSorter
+
+VIR_OUT_DIR=$1
+
+cd $VIR_OUT_DIR
+
+# Remove previous file if present
+if [[ -f "combined_sequences.txt" ]]; then
+    rm "combined_sequences.txt"
+fi
+
+for prefix in "VIRSorter_cat-" "VIRSorter_prophages_cat-"; do
+    # File name category number depends on prefix
+    if [ $prefix == 'VIRSorter_cat-' ]; then
+        categories=(1 2 3)
+    else
+        categories=(4 5 6)
+    fi
+
+    for category in ${categories[@]}; do
+        file=${prefix}${category}.fasta
+
+        grep ">" $file |
+        sed 's/>//' |
+        sed 's/VIRSorter_//' >> combined_sequences.txt
+    done
+done
