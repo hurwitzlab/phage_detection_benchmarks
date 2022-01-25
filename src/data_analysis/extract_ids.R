ec8ee10a05de1c65789ad3b3fe1231a3905b474b
Kenneth Edward Schackart <schackartk@email.arizona.edu>
2022-01-25 13:25:14 -0700

    add extract_ids.R

---
+++ src/data_analysis/extract_ids.R
@@ -0,0 +1,90 @@
+#!/usr/bin/env Rscript
+
+# Author : Kenneth Schackart <schackartk1@gmail.com>
+# Date   : 2022-01-25
+# Purpose: Extract Assembly Accession and Record IDs
+
+# Imports -------------------------------------------------------------------
+
+## Library calls ------------------------------------------------------------
+
+library(dplyr)
+library(magrittr)
+library(stringr)
+
+# Argument Parsing ----------------------------------------------------------
+
+#' Parse Arguments
+#'
+#' Parse command line arguments using argparse.
+#'
+#' @return args
+get_args <- function() {
+  parser <-
+    argparse::ArgumentParser(description = "Extract Assembly Accession and Record IDs")
+  
+  parser$add_argument("file",
+                      help = "File resulting from grep '>' on genome files",
+                      metavar = "FILE")
+  parser$add_argument("-o",
+                      "--outdir",
+                      help = "Output directory",
+                      type = "character",
+                      default = "out")
+  
+  args <- parser$parse_args()
+  
+  return(args)
+  
+}
+
+# Main ----------------------------------------------------------------------
+
+#' Main Function
+#'
+#' @return
+main <- function() {
+  args <- get_args()
+  file <- args$file
+  out_dir <- args$outdir
+  
+  if (file.access(file) == -1) {
+    stop(str_glue("Specified file '{file}' cannot be accessed."))
+  } else {
+    ids <- readLines(file)
+  }
+  
+  if (!dir.exists(out_dir)) {
+    dir.create(out_dir)
+  }
+  
+  out_file <-  file.path(out_dir, "extracted_ids.csv")
+  
+  expression <- paste0(
+    "([[:alpha:]]+)/",
+    # kingdom
+    "([[:alpha:]]{3}_[[[:alnum:]]\\.]+)_.*",
+    # accession
+    ">([[[:alnum:]]_\\.]+)\\s.*"             # seq_id
+  )
+  
+  taxonomy <- ids %>%
+    str_match(expression) %>%
+    as_tibble() %>%
+    rename(
+      original = "V1",
+      kingdom = "V2",
+      accesssion = "V3",
+      seq_id = "V4"
+    ) %>%
+    select(-original)
+  
+  write.csv(taxonomy, out_file, row.names = FALSE)
+  
+  print(str_glue("Done. Wrote file to '{out_file}'"))
+}
+
+# Call Main -----------------------------------------------------------------
+if (!interactive()) {
+  main()
+}
