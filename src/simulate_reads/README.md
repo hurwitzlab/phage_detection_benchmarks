f5ebc46ad6ed240eb416b637c7d6c16357b4203a
Kenneth Schackart <schackartk1@gmail.com>
2022-02-09 13:01:39 -0700

    add first vresion of braken_profiler.py

--- src/simulate_reads/README.md
+++ src/simulate_reads/README.md
@@ -14,3 +14,31 @@ InSiicoSeq treats each record in a multi-FASTA file as a genome. However, some g
 ### Read Simulation
 
 Several abundance profiles can be created, and the specified genomes must have identifiers that match those in `cat_genomes.fasta`. The list of desired abundance profiles should be given in the `--configfile`, as well as other `iss generate` parameters. Multiple error models can be automatically executed by inclusion in the config["model"] field, which currently includes all pre-built error models.
+
+
+## `braken_profiler.py`
+
+This script takes Braken output and creates a profile for use in ISS. Taxonomic IDs are used to join the Braken output with my list of refseq genomes. Since not all genomes may be found, the abundances are rescaled so they still add to 1. The abundance profile is written to `*_profile.txt`.
+
+Additionally, a file is created (`*_files.txt`) that contains a list of "file names" (file globs that should only match one file). The files that match these globs are the files that contain the sequences in the profile.
+
+Example usage
+```
+ $ ./braken_profiler.py tests/inputs/braken_profiler/input_1.txt
+Done. Wrote output files to "out".
+
+$ ls out/
+input_1_files.txt  input_1_profile.txt
+
+# File globs do not include the parent directory (refseq)
+$ head -n 3 out/input_1_files.txt 
+bacteria/GCF_006742345.1*.fna
+bacteria/GCF_004328515.1*.fna
+bacteria/GCF_015679285.1*.fna
+
+# First column is sequence ID, so will still need to use cat_genomes.sh
+$ head -n 3 out/input_1_profile.txt 
+NZ_AP019724.1   0.1027
+NZ_CP036491.1   0.0189
+NZ_CP064937.1   0.01957
+```
--- src/simulate_reads/braken_profiler.py
+++ src/simulate_reads/braken_profiler.py
@@ -6,16 +6,18 @@ Purpose: Create profile from Braken output
 """
 
 import argparse
-from typing import NamedTuple, TextIO
+import os
+import pandas as pd
+from typing import NamedTuple, TextIO, Tuple
+
+pd.options.mode.chained_assignment = None
 
 
 class Args(NamedTuple):
     """ Command-line arguments """
-    positional: str
-    string_arg: str
-    int_arg: int
-    file: TextIO
-    on: bool
+    braken: TextIO
+    taxonomy: TextIO
+    outdir: str
 
 
 # --------------------------------------------------
@@ -31,53 +33,148 @@ def get_args() -> Args:
                         help='Braken output file',
                         type=argparse.FileType('rt'))
 
-    parser.add_argument('-a',
-                        '--arg',
-                        help='A named string argument',
-                        metavar='str',
-                        type=str,
-                        default='')
-
-    parser.add_argument('-i',
-                        '--int',
-                        help='A named integer argument',
-                        metavar='int',
-                        type=int,
-                        default=0)
-
-    parser.add_argument('-f',
-                        '--file',
-                        help='A readable file',
+    parser.add_argument('-t',
+                        '--taxonomy',
                         metavar='FILE',
+                        help='Taxonomy mapping file',
                         type=argparse.FileType('rt'),
-                        default=None)
+                        default='../../data/refseq_info/taxonomy.csv')
 
     parser.add_argument('-o',
-                        '--on',
-                        help='A boolean flag',
-                        action='store_true')
+                        '--outdir',
+                        metavar='DIR',
+                        help='Output directory',
+                        type=str,
+                        default='out')
 
     args = parser.parse_args()
 
-    return Args(args.positional, args.arg, args.int, args.file, args.on)
+    return Args(args.braken, args.taxonomy, args.outdir)
 
 
 # --------------------------------------------------
 def main() -> None:
-    """ Make a jazz noise here """
+    """ Do the stuff """
 
     args = get_args()
-    str_arg = args.string_arg
-    int_arg = args.int_arg
-    file_arg = args.file
-    flag_arg = args.on
-    pos_arg = args.positional
+    out_dir = args.outdir
 
-    print(f'str_arg = "{str_arg}"')
-    print(f'int_arg = "{int_arg}"')
-    print('file_arg = "{}"'.format(file_arg.name if file_arg else ''))
-    print(f'flag_arg = "{flag_arg}"')
-    print(f'positional = "{pos_arg}"')
+    if not os.path.isdir(out_dir):
+        os.mkdir(out_dir)
+
+    braken_df = clean_braken(pd.read_csv(args.braken, sep='\t'))
+    taxonomy_df = clean_taxonomy(pd.read_csv(args.taxonomy))
+
+    joined_df = join_dfs(braken_df, taxonomy_df)
+
+    joined_df['rescaled_abundance'] = rescale_abundances(
+        joined_df['fraction_total_reads'])
+
+    files_df = make_files_df(joined_df)
+    profile_df = make_profile_df(joined_df)
+
+    files_output, profile_output = make_filenames(args.braken.name, out_dir)
+
+    files_df.to_csv(files_output, sep=",", index=False, header=False)
+    profile_df.to_csv(profile_output, sep="\t", index=False, header=False)
+
+    print(f'Done. Wrote output files to "{out_dir}".')
+
+
+# --------------------------------------------------
+def clean_braken(df: pd.DataFrame) -> pd.DataFrame:
+    """ Clean braken output dataframe """
+
+    df = df[df['fraction_total_reads'] > 0.]
+
+    df.drop([
+        'taxonomy_lvl', 'kraken_assigned_reads', 'added_reads', 'new_est_reads'
+    ],
+            axis='columns',
+            inplace=True)
+
+    df.sort_values('fraction_total_reads')
+
+    return df
+
+
+# --------------------------------------------------
+def clean_taxonomy(df: pd.DataFrame) -> pd.DataFrame:
+    """ Clean taxonomy dataframe """
+
+    df = df[['kingdom', 'accession', 'taxid', 'seq_id']]
+
+    # There may be multiple reseq files (accession #)
+    # for a given tax_id. I need tax_id to be unique,
+    # so I will drop duplicate tax id's, keeping only the
+    # first accession.
+    df = df.drop_duplicates('taxid')
+
+    return df
+
+
+# --------------------------------------------------
+def join_dfs(braken: pd.DataFrame, tax: pd.DataFrame) -> pd.DataFrame:
+    """ Join braken and taxonomy dfs """
+
+    joined_df = pd.merge(braken,
+                         tax,
+                         how='inner',
+                         left_on='taxonomy_id',
+                         right_on='taxid')
+
+    return joined_df
+
+
+# --------------------------------------------------
+def rescale_abundances(col: pd.Series) -> pd.Series:
+    """ Rescale abundances to add to 1 """
+
+    rescale_factor = 1 / col.sum()
+
+    rescaled = rescale_factor * col
+
+    return rescaled
+
+
+# --------------------------------------------------
+def make_files_df(df: pd.DataFrame) -> pd.DataFrame:
+    """ Create file names of genomes """
+
+    files_df = df[['kingdom', 'accession']]
+
+    files_df['filename'] = files_df['kingdom'] + '/' + files_df[
+        'accession'] + '*.fna'
+
+    files_df = files_df[['filename']]
+
+    return files_df
+
+
+# --------------------------------------------------
+def make_profile_df(df: pd.DataFrame) -> pd.DataFrame:
+    """ Create file of seq IDs and abundances """
+
+    profile_df = df[['seq_id', 'rescaled_abundance']]
+
+    profile_df['rescaled_abundance'] = round(profile_df['rescaled_abundance'],
+                                             5)
+
+    return profile_df
+
+
+# --------------------------------------------------
+def make_filenames(infile: str, out_dir: str) -> Tuple[str, str]:
+    """ Create names of output files """
+
+    base = os.path.basename(infile)
+
+    root, _ = os.path.splitext(base)
+
+    files_output = os.path.join(out_dir, root + '_files.txt')
+    profile_output = os.path.join(out_dir, root + '_profile.txt')
+
+    return files_output, profile_output
 
 
 # --------------------------------------------------
