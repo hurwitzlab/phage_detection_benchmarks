00cb8c1e1babc1f8d273e7b4d8591f9c08631842
Kenneth Schackart <schackartk1@gmail.com>
2022-02-18 11:38:16 -0700

    add runs_okay

--- src/simulate_reads/tests/bracken_profiler_test.py
+++ src/simulate_reads/tests/bracken_profiler_test.py
@@ -3,25 +3,26 @@
 import os
 import random
 import re
+import shutil
 import string
 from subprocess import getstatusoutput
 
-PRG = "./bracken_profiler.py"
-INPUT1 = "tests/inputs/bracken_profiler/input_1.txt"
-INPUT2 = "tests/inputs/bracken_profiler/input_1.txt"
-TAX = "tests/inputs/bracken_profiler/input_1.txt"
+PRG = './bracken_profiler.py'
+INPUT1 = 'tests/inputs/bracken_profiler/input_1.txt'
+INPUT2 = 'tests/inputs/bracken_profiler/input_1.txt'
+TAX = 'tests/inputs/bracken_profiler/input_1.txt'
 
 
 # --------------------------------------------------
 def test_exists():
-    """Program exists"""
+    """ Program exists """
 
     assert os.path.isfile(PRG)
 
 
 # --------------------------------------------------
 def test_testing_environment():
-    """Test files are in place"""
+    """ Test files are in place """
 
     assert os.path.isfile(INPUT1)
     assert os.path.isfile(INPUT2)
@@ -30,42 +31,72 @@ def test_testing_environment():
 
 # --------------------------------------------------
 def test_usage():
-    """Usage"""
+    """ Usage """
 
-    for flag in ["-h", "--help"]:
-        retval, out = getstatusoutput(f"{PRG} {flag}")
+    for flag in ['-h', '--help']:
+        retval, out = getstatusoutput(f'{PRG} {flag}')
         assert retval == 0
-        assert out.lower().startswith("usage")
+        assert out.lower().startswith('usage')
 
 
 # --------------------------------------------------
 def test_bad_file():
-    """Bad input file"""
+    """ Bad input file """
 
     bad = random_string()
 
-    retval, out = getstatusoutput(f"{PRG} -t {TAX} {bad}")
+    retval, out = getstatusoutput(f'{PRG} -t {TAX} {bad}')
     assert retval != 0
-    assert out.lower().startswith("usage:")
-    assert re.search("No such file", out)
+    assert out.lower().startswith('usage:')
+    assert re.search('No such file', out)
     assert re.search(bad, out)
 
 
 # --------------------------------------------------
 def test_bad_taxonomy_file():
-    """Bad input file"""
+    """ Bad input file """
 
     bad = random_string()
 
-    retval, out = getstatusoutput(f"{PRG} -t {bad} {INPUT1}")
+    retval, out = getstatusoutput(f'{PRG} -t {bad} {INPUT1}')
     assert retval != 0
-    assert out.lower().startswith("usage:")
-    assert re.search("No such file", out)
+    assert out.lower().startswith('usage:')
+    assert re.search('No such file', out)
     assert re.search(bad, out)
 
 
 # --------------------------------------------------
-def random_string() -> str:
-    """Generate a random string"""
+def test_runs_okay():
+    """ Run the tool """
 
-    return "".join(random.choices(string.ascii_uppercase + string.digits, k=5))
+    out_dir = random_string()
+
+    try:
+        if os.path.isdir(out_dir):
+            shutil.rmtree(out_dir)
+
+        rv, out = getstatusoutput(f'{PRG} -o {out_dir} {INPUT1}')
+
+        assert rv == 0
+        glob_file = os.path.join(out_dir, 'input_1_files.txt')
+        profile_file = os.path.join(out_dir, 'input_1_profile.txt')
+        assert re.search(f'Done. Wrote 1 profile to {out_dir}', out)
+        assert os.path.isdir(out_dir)
+        assert os.path.isfile(glob_file)
+        assert os.path.isfile(profile_file)
+        header = ('filename,seq_id\n')
+        assert open(glob_file).readlines()[0] == header
+        profile_lines = open(profile_file).read().count('\n')
+        glob_lines = open(glob_file).read().count('\n')
+        assert glob_lines == profile_lines + 1
+
+    finally:
+        if os.path.isdir(out_dir):
+            shutil.rmtree(out_dir)
+
+
+# --------------------------------------------------
+def random_string() -> str:
+    """ Generate a random string """
+
+    return ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
