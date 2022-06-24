cffa25bc0542c279e3ea8976dffb5d7edb8f3f20
Kenneth Schackart <schackartk1@gmail.com>
2022-06-24 15:36:12 -0700

    exclude non-phage viruses

--- src/classify_simulated/get_summary_stats.py
+++ src/classify_simulated/get_summary_stats.py
@@ -267,6 +267,7 @@ def add_taxonomy(preds: pd.DataFrame, tax: pd.DataFrame) -> pd.DataFrame:
                       right_on='query_id')
 
     out_df = out_df[out_df['superkingdom'].notna()]
+    out_df = out_df[out_df['species'].notna()]
     out_df = out_df[out_df['assembly'] == 'single']
 
     out_df = out_df[[
@@ -276,8 +277,19 @@ def add_taxonomy(preds: pd.DataFrame, tax: pd.DataFrame) -> pd.DataFrame:
 
     out_df = out_df.rename(columns={'query_length': 'length'})
 
-    out_df['actual_class'] = np.where(out_df['superkingdom'] == 'Viruses',
-                                      'viral', 'non-viral')
+    # Phages are viruses with phage in the species name
+    out_df['actual_class'] = np.where(
+        out_df['superkingdom'] == 'Viruses'
+        and out_df['species'].str.contains('phage', case=False), 'viral',
+        'non-viral')
+
+    # Remove non-phage viruses
+    out_df['actual_class'] = np.where(
+        out_df['superkingdom'] == 'Viruses'
+        and not out_df['species'].str.contains('phage', case=False), np.nan,
+        out_df['actual_class'])
+
+    out_df = out_df.dropna('actual_class')
 
     return out_df
 
@@ -415,7 +427,7 @@ def main() -> None:
     metrics = get_tool_metrics(predictions)
 
     metrics.to_csv(out_file, index=False)
-    
+
     print(f'Done. Wrote to file {out_file}.')
 
 
